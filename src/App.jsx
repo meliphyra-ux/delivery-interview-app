564cebf413fc008e0c20c43edb02d0a5d2ae5c8c
meliphyra-ux <xennerit@icloud.com>
2023-03-28 16:16:26 +0300

    Hot fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
 import { Suspense, lazy, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 import { createUserFromAuth0, getOrders } from './firebase/app';
+
+import { loadOrders } from './store/orders/ordersActions';
 
 import Navigation from './roots/navigation/Navigation';
 
 const Home = lazy(() => import('./roots/home/Home'));
 const MyOrders = lazy(() => import('./roots/my-orders/My-orders'));
 const Cart = lazy(() => import('./roots/cart/Cart'));
+const ModalWindow = lazy(() =>
+  import('./components/modal-window/Modal-window')
+);
+const OrderDetails = lazy(() => import('./roots/order-details/Order-details'));
 
 import './App.scss';
-import ModalWindow from './components/modal-window/Modal-window';
-import OrderDetails from './roots/order-details/Order-details';
-import { useDispatch } from 'react-redux';
-import { loadOrders } from './store/orders/ordersActions';
 
 const App = () => {
   const { user } = useAuth0();
   const dispatch = useDispatch();
   useEffect(() => {
     if (user && user.email) {
-      console.log('Settled')
+      console.log('Settled');
       Promise.allSettled([
         getOrders(user.email),
         createUserFromAuth0(user),
@@ -47,8 +50,8 @@ const App = () => {
             <Route path="cart" element={<Cart />} />
           </Route>
         </Routes>
-      </Suspense>
-      <ModalWindow />
+        <ModalWindow />
+      </Suspense> 
     </BrowserRouter>
   );
 };
